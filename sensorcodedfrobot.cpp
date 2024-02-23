b47c70a4be27b34654ba2f8aad11ff92c4319630
MathiasDeCr <159553271+MathiasDeCr@users.noreply.github.com>
2024-02-23 15:00:34 +0100

    Update sensorcodedfrobot.cpp
    3 sensors

--- sensorcodedfrobot.cpp
+++ sensorcodedfrobot.cpp
@@ -17,21 +17,31 @@
 // #        Indicator LED -> Digital pin 13
 // #
 
-byte sensorPin = 2;
-byte indicator = 13;
+byte sensorPin1 = D2;
+byte sensorPin2 = D3;
+byte sensorPin3 = D4;
 
 void setup()
 {
-  pinMode(sensorPin,INPUT);
-  pinMode(indicator,OUTPUT);
+  pinMode(sensorPin1,INPUT);
+  pinMode(sensorPin2,INPUT);
+  pinMode(sensorPin3,INPUT);
+
   Serial.begin(9600);
 }
 
 void loop()
 {
-  byte state = digitalRead(sensorPin);
-  digitalWrite(indicator,state);
-  if(state == 1)Serial.println("Somebody is in this area!");
-  else if(state == 0)Serial.println("No one!");
+  byte state1 = digitalRead(sensorPin1);
+  if(state1 == 1)Serial.println("Somebody is in this area of the first sensor!");
+  else if(state1 == 0)Serial.println("No one at first sensor!");
+  delay(500);
+  byte state2 = digitalRead(sensorPin2);
+  if(state2 == 1)Serial.println("Somebody is in this area of the second sensor!");
+  else if(state2 == 0)Serial.println("No one at second sensor!");
+  delay(500);
+  byte state3 = digitalRead(sensorPin3);
+  if(state3 == 1)Serial.println("Somebody is in this area of the third sensor!");
+  else if(state3 == 0)Serial.println("No one at third sensor!");
   delay(500);
 }
